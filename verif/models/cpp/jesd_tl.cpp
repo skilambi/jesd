2ea1b010838423965408edd7cc53e4b56368d53a
Sai Mohan Kilambi <skilambi@leapfrogsemi.com>
2023-11-15 16:29:00 -0600

    nibble group data structure moved into the class

--- verif/models/cpp/jesd_tl.cpp
+++ verif/models/cpp/jesd_tl.cpp
@@ -10,20 +10,42 @@ class JesdTl {
         uint32_t Np; // JESD TL Precision
         uint32_t R; // Rate of the input
 
+        int num_samples; // This is the number of samples that this block will process
+                         // Allows us to define internal data structures of the right size.
+                         // In real hardware this would just be a streaming block.
+        
+        uint64_t** ng_data;   // This is a 2D array that will store converter raw samples to word mapping.
+                                // We define this as 64 bit so that we can accomodate max 48 bit support.
+
         // Constructors
         JesdTl () {
             setL(2);
             setM(2);
             setNp(16);
             setR(1);
+            setNumSamp(16);
+            
+            // Create a 2-D array for converter raw word to word mapping
+            ng_data = (uint64_t**) malloc(getM() * sizeof(uint64_t));
+
+            for ( int i = 0; i < getM(); i++) {
+                ng_data[i] = (uint64_t*) malloc(num_samples * sizeof(uint64_t));
+            } 
         }
 
-        JesdTl ( uint32_t L, uint32_t M, uint32_t Np, uint32_t R) {
+        JesdTl ( uint32_t L, uint32_t M, uint32_t Np, uint32_t R, int num_samples) {
             setL ( L );
             setM ( M );
             setNp ( Np );
             setR ( R );
+            setNumSamp ( num_samples );
 
+            // Create a 2-D array for converter raw word to word mapping
+            ng_data = (uint64_t**) malloc(getM() * sizeof(uint64_t));
+
+            for ( int i = 0; i < getM(); i++) {
+                ng_data[i] = (uint64_t*) malloc(num_samples * sizeof(uint64_t));
+            } 
         }
 
         // Getter and Setter Methods
@@ -53,6 +75,10 @@ class JesdTl {
         void setNp ( uint32_t Np ) { 
             this->Np = Np;
         }
+
+        void setNumSamp ( int num_samples ) { 
+            this->num_samples = num_samples;
+        }
         
         uint32_t getM ( ) { 
             return M * P;
@@ -73,12 +99,12 @@ class JesdTl {
         // Logic Methods
 
         /*  
-            Function: map_cw_2_w
+            Function: map_cw_2_ng
             Description:
-            The purpose of this function is convert convertor words (which are the raw converter samples) to words
+            The purpose of this function is convert convertor words (which are the raw converter samples) to nibble groups (ng)
             as described in Fig 38 of the JESD204C document. Convertor words can be either 12 bits or 16 bits. These
-            can be mapped to words that are of size 12, 16, 24, 32 or 48. So the purpose of this function is to expand
-            the raw samples to words depending on the Np setting of this class. 
+            can be mapped to ng that are of size 12, 16, 24, 32 or 48. So the purpose of this function is to expand
+            the raw samples to nibble group depending on the Np setting of this class. 
             Arguments: 
             raw_conv_data: 2D array where the number of rows is the number of converters times phases and columns is
                 number of samples (valid or invalid). Note that raw converter samples can either be 12 bit or 16 bit.
@@ -90,33 +116,33 @@ class JesdTl {
             
             conv_prec: Precision of the converters (12 or 16)
 
-            word_data: This is the output of this function. Samples in the raw_conv_data array will be expanded to Np. 
+            ng_data: This is the output of this function. Samples in the raw_conv_data array will be expanded to Np. 
                 The number of rows in this array will be known apriori (equal to M x P).
         */
-        void map_cw_2_w ( uint16_t** raw_conv_data, int col, int conv_prec, uint64_t** word_data ) {
+        void map_cw_2_w ( uint16_t** raw_conv_data ) {
             int num_row = M * P;
 
             for ( int r = 0; r < num_row; r ++ ) {
-                for ( int c = 0; c < col; c ++) {
+                for ( int c = 0; c < num_samples; c ++) {
                     switch ( Np ) {
                         case 12: {
-                            word_data[r][c] =  ( (uint64_t) raw_conv_data[r][c] ) >> 4;
+                            ng_data[r][c] =  ( (uint64_t) raw_conv_data[r][c] ) >> 4;
                             break;
                         }
                         case 16: {
-                            word_data[r][c] =  ( (uint64_t) raw_conv_data[r][c] );
+                            ng_data[r][c] =  ( (uint64_t) raw_conv_data[r][c] );
                             break;
                         }
                         case 24: {
-                            word_data[r][c] =  ( (uint64_t) raw_conv_data[r][c] ) << 8;
+                            ng_data[r][c] =  ( (uint64_t) raw_conv_data[r][c] ) << 8;
                             break;   
                         }
                         case 32: {
-                            word_data[r][c] =  ( (uint64_t) raw_conv_data[r][c] ) << 16;
+                            ng_data[r][c] =  ( (uint64_t) raw_conv_data[r][c] ) << 16;
                             break;   
                         }
                         case 48: {
-                            word_data[r][c] = ( (uint64_t) raw_conv_data[r][c] ) << 32;
+                            ng_data[r][c] = ( (uint64_t) raw_conv_data[r][c] ) << 32;
                             break;   
                         }
                     }
@@ -126,8 +152,8 @@ class JesdTl {
             // The following is just for debug. Comment out when not needed
             // Now print all the samples 
             for ( int m = 0; m < num_row; m ++ ) {
-                for ( int s = 0; s < col; s ++ ) {
-                    cout << hex  << setw(18) << word_data[m][s] << " ";
+                for ( int s = 0; s < num_samples; s ++ ) {
+                    cout << hex  << setw(18) << ng_data[m][s] << " ";
                 }
                 cout << endl;
             }   
@@ -158,13 +184,13 @@ class JesdTl {
             EXTERNAL FUNCTIONS (not part of class methods)
    ==========================================================    */
 
-void adj_input_data_dim(uint32_t* col, JesdTl obj, int num_samp){
+void adj_input_data_dim(uint32_t* col, uint16_t R, int num_samp){
     // Depending on the Rate we want to insert dummy values
     // that are not valid samples. Hence we need to define a new
     // number of samples.
     uint32_t mod_num_samp;
 
-    switch ( obj.getR() ){
+    switch ( R ){
         case 1: { // 122.88 MSps
             mod_num_samp = num_samp * 4;
             break;
@@ -248,9 +274,6 @@ int main(int argc, char *argv[]) {
     uint32_t num_samp = 12;
 
 
-    // Create the JESDTL object
-    JesdTl tlobj(L, M, Np, R);
-
     // We will now prepare the input data. In the model
     // the number of rows will be the number of converters
     // we are defining. The number of columns will be the
@@ -258,7 +281,12 @@ int main(int argc, char *argv[]) {
     // will need to be adjusted based on the rate
     uint32_t adj_num_samp ;
     // Adjust the number of input samples based on rate
-    adj_input_data_dim(&adj_num_samp, tlobj, num_samp);
+    adj_input_data_dim(&adj_num_samp, R, num_samp);
+
+    // Create the JESDTL object. Need to create this
+    // before the inp_data array creation.
+    JesdTl tlobj(L, M, Np, R, adj_num_samp);
+
 
     // Now create a 2-D array where the rows are converters
     // and the columns are input samples for each converter.
@@ -272,6 +300,7 @@ int main(int argc, char *argv[]) {
     // Generate random input data
     gen_conv_data ( inp_data, tlobj.getM(), adj_num_samp, tlobj.getR());
 
+    // Print this input array for debug.
     cout << showbase << internal << setfill('0');
     // Now print all the samples 
     for ( int m = 0; m < tlobj.getM(); m ++ ) {
@@ -281,15 +310,7 @@ int main(int argc, char *argv[]) {
         cout << endl;
     }
 
-    // Create a 2-D array for converter raw word to word mapping
-    uint64_t** word_data;
-    word_data = (uint64_t**) malloc(tlobj.getM() * sizeof(uint64_t));
-
-    for ( int i = 0; i < tlobj.getM(); i++) {
-        word_data[i] = (uint64_t*) malloc(adj_num_samp * sizeof(uint64_t));
-    } 
-
-    tlobj.map_cw_2_w(inp_data, adj_num_samp, 16, word_data);
+    tlobj.map_cw_2_w( inp_data );
 
     /*
     cout << "Number of arguments: " << argc << endl;
@@ -307,7 +328,6 @@ int main(int argc, char *argv[]) {
     cout << "Number of Lanes: " << tlobj.getM() << endl;
 
     free(inp_data);
-    free(word_data);
 
     return 0;
 }
